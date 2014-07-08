154eddabf8a5c4bf27459142cf2eacd046e1b5b0
mjdonovan410 <mjdonovan410@gmail.com>
2014-07-08 13:43:35 -0400

    Old file

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from Tkinter import Tk
-from tkFileDialog import askopenfilename, asksaveasfilename
-
-Tk().withdraw()
-infile = askopenfilename(filetypes=[("H264 Video","*.h264")])
-os.system("avconv -i "+infile+" -r 25 -f image2 pic_temp/%05d.jpg")
-os.system("rm -rf ./pic_temp")
