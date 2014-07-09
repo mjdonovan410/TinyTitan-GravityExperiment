a74a015791a4b73dd5866dcfbebaf6731cb8536c
mjdonovan410 <mjdonovan410@gmail.com>
2014-07-09 10:20:58 -0400

    Added more keyboard functionality

--- exp.py
+++ exp.py
@@ -1,11 +1,20 @@
+'''
+Author:   Matthew Donovan
+Created:  7/8/14
+File:     exp.py
+Purpose:  Provides a service to load a video created by the raspberry pi, and convert the visuals into data points to be used
+in other programs.
+'''	
+
 import pygame, pickle
 from functions import *
 from button import *
+from textrect import render_textrect
 from pygame.locals import *
 
 pygame.init()
 
-# Screen and System Variables
+# Screen and Basic Program Variables
 clock = pygame.time.Clock()
 screen_size = (720,720)
 screen = pygame.display.set_mode(screen_size)
@@ -13,30 +22,32 @@ frame_range = [0,0]
 frame_size = (480,640)
 frame_loc = (screen_size[0]-(frame_size[0]+10),10)
 frames = []
+keyFrames = []
 logo = pygame.image.load("Images/ORNL_Images/ORNL_Stacked_white_banner.png")
 bg = pygame.image.load("Images/bg.png")
 pic_temp = pygame.image.load("Images/pic_temp.png")
 crosshairs = [pygame.image.load("Images/crosshairs_w.png"),pygame.image.load("Images/crosshairs_h.png")]
 dotR = pygame.image.load("Images/dotr.png").convert_alpha()
 dotB = pygame.image.load("Images/dotb.png").convert_alpha()
-font = pygame.font.SysFont("sanserif",36)
-font2 = pygame.font.SysFont("sanserif",24)
+fontB = pygame.font.SysFont("sanserif",36)
+fontL = pygame.font.SysFont("sanserif",24)
+numberBox = [None,None]
+number_rect = pygame.Rect((0,0), (100,40))
 curFrame = 0
 timer = 0
-keyFrames = []
 button_str = ""
 message_str = ""
-message_rect = pygame.Rect((0,0), (200,150))
+message_rect = pygame.Rect((0,0), (200,50))
 showPoints = False
 mouseOnPic = False
 message = False
 
-# Buttons for rendering
+# Creates an array of Button objects the will provide data and blit the objects to the screen
 buttons = []
 buttons = load_buttons(buttons, screen, screen_size)
 
-# Video Frame Setup
-update_rate = 25
+# Basic GUI Frame Setup
+update_rate = 40
 screen.blit(bg,(0,0))
 screen.blit(logo,(0,0))
 screen.blit(pic_temp,frame_loc)
@@ -44,21 +55,25 @@ screen.blit(pic_temp,frame_loc)
 while True:
 	prevFrame = curFrame
 	x,y = pygame.mouse.get_pos()
+	
+	#Checks if the mouse is hovering over the image so the crosshairs can be added and reduce updating frequency
 	if x in range(frame_loc[0],frame_loc[0]+frame_size[0]) and y in range(frame_loc[1],frame_loc[1]+frame_size[1]):
 		mouseOnPic = True
 	else:
 		mouseOnPic = False
-		
-	for i in buttons:
-		i.mouseloc(x,y)
-		
+		for i in buttons: # Checks if mouse is hovering over a button and changes the image accordingly
+			i.mouseloc(x,y)
+	
+	# Checks for inputs whether it be a mouse click, key pressed, or the 'X' has been pressed in the corner
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exitAndClean()
+		
+		# Keyboard shortcuts for the action buttons
 		elif event.type == KEYDOWN:
-			if event.key == K_LEFT:
+			if event.key == K_LEFT or event.key == K_a:
 				curFrame = left_key(curFrame,frame_range)
-			elif event.key == K_RIGHT:
+			elif event.key == K_RIGHT or event.key == K_d:
 				curFrame = right_key(curFrame,frame_range)
 			elif event.key == K_s:
 				frame_range[0] = curFrame
@@ -68,25 +83,39 @@ while True:
 				button_str = "set_end"
 			elif event.key == K_c:
 				frame_range = [0,len(frames)-1]
+				button_str = "reset"
 			elif event.key == 27:
 				exitAndClean()
+			elif event.key == K_q:
+				curFrame = button_pressed(screen,curFrame,"skipb",frame_range)
+			elif event.key == K_e:
+				curFrame = button_pressed(screen,curFrame,"skipf",frame_range)
+				
+		# Used for both buttons being pressed or location of a data point
 		elif event.type == MOUSEBUTTONDOWN:
 			x,y = event.pos
 			if mouseOnPic and frames != []:
 				keyFrames[curFrame] = (x-screen_size[0]+(frame_size[0]+10),y-10)
 			else:
+				#Checks if a button has been pressed
 				for i in buttons:
 					if i.mouseloc(x,y):
 						button_str = i.getactionStr()
 						temp = button_pressed(screen,curFrame,button_str,frame_range)
+						
+						# Loads a new video to the screen and clears the keyFrames from the previous video
 						if button_str == 'load':
-							if temp != None:
+							if temp is not None:
 								frames = temp
 								frame_range[1] = len(frames)-1
+								numberBox[0] = render_textrect(str(frame_range[0]), fontB, number_rect, (255,255,255), (0,0,0), justification=1)
+								numberBox[1] = render_textrect(str(frame_range[1]), fontB, number_rect, (255,255,255), (0,0,0), justification=1)
 								for p in range(len(frames)):
 									keyFrames.append((1000,1000))
-								update_all(screen, screen_size, frames, frame_loc, frame_range, curFrame, font)
+								update_all(screen, screen_size, frames, frame_loc, numberBox, curFrame, fontB)
 								curFrame = 0
+						
+						# These functions only work when there are frames loaded
 						if frames != []:
 							if button_str in ['stepf','stepb','skipf','skipb','start','end']:
 								curFrame = temp
@@ -100,53 +129,68 @@ while True:
 									showPoints = False
 								else:
 									showPoints = True
-								update_pic(screen, screen_size, frames, frame_loc, curFrame, font)
+								update_pic(screen, screen_size, frames, frame_loc, curFrame, fontB)
+							
+							# Saves a file to be used in another program for processing
 							elif button_str == 'save':
-								message_area, message, curFrame = save_file(keyFrames, frame_range, curFrame, font2)
-									
+								message_area, message, curFrame = save_file(keyFrames, frame_range, curFrame, fontL)
+							
+							# Clears all the data points from the frames
 							elif button_str == 'clear':
 								keyFrames = clear_points(keyFrames)
-								update_pic(screen, screen_size, frames, frame_loc, curFrame, font)
+								update_pic(screen, screen_size, frames, frame_loc, curFrame, fontB)
 								message_str = "Data Points Erased"
 								message = True
-								message_area = render_textrect(message_str, font2, message_rect, (255,0,0), (0,0,0), justification=1)
-								
+								message_area = render_textrect(message_str, fontL, message_rect, (255,0,0), (0,0,0), justification=1)
+							
+							elif button_str == 'reset':
+								frame_range = [0,len(frames)-1]								
 	
-	frame_num = font.render(str(curFrame),1,(255,255,255))
+	frame_num = fontB.render(str(curFrame),1,(255,255,255))
 	
+	# Updates the video frames on the right with the appropriate layering
 	if prevFrame != curFrame and frames != []:
-		update_pic(screen, screen_size, frames, frame_loc, curFrame, font)
+		update_pic(screen, screen_size, frames, frame_loc, curFrame, fontB)
 		if keyFrames[curFrame] != (1000,1000):
 			temp = keyFrames[curFrame]
 			screen.blit(dotR,(frame_loc[0]-5+temp[0],frame_loc[1]-5+temp[1]))
 	
-	if button_str in ["set_end","set_start"] and frames != []:
-		update_all(screen, screen_size, frames, frame_loc, frame_range, curFrame, font)
+	# Updates the whole screen with the new start and end points
+	if button_str in ["set_end","set_start","reset"] and frames != []:
+		numberBox[0] = render_textrect(str(frame_range[0]), fontB, number_rect, (255,255,255), (0,0,0), justification=1)
+		numberBox[1] = render_textrect(str(frame_range[1]), fontB, number_rect, (255,255,255), (0,0,0), justification=1)
+		screen.blit(numberBox[0],(10,265))
+		screen.blit(numberBox[1],(120,265))
 		message = True
 		if button_str == "set_end":
 			message_str = "End Point set at Frame "+str(curFrame)
-		else:
+		elif button_str == "set_start":
 			message_str = "Start Point set at Frame "+str(curFrame)
-		message_area = render_textrect(message_str, font2, message_rect, (255,0,0), (0,0,0), justification=1)
-		
+		else:
+			message_str = "Start and End Points Reset"
+		message_area = render_textrect(message_str, fontL, message_rect, (255,0,0), (0,0,0), justification=1)
+	
+	# Updates the button images if they are being hovered
 	update_buttons(screen, buttons)
 	
-	if mouseOnPic:
-		if frames != []:
-			crosshair_loc = font.render("("+str((x-screen_size[0]+(frame_size[0]+10)))+","+str(y-10)+")",1,(255,255,255))
-			update_pic(screen, screen_size, frames, frame_loc, curFrame, font)
-			screen.blit(crosshairs[1],(x,10))
-			screen.blit(crosshairs[0],(screen_size[0]-(frame_size[0]+10),y))
-			screen.blit(crosshair_loc,(screen_size[0]-(frame_size[0]),screen_size[1]-100))
+	# Adds crosshairs if the mouse is on the frame and there are frames
+	if mouseOnPic and frames != []:
+		crosshair_loc = fontB.render("("+str((x-screen_size[0]+(frame_size[0]+10)))+","+str(y-10)+")",1,(255,255,255))
+		update_pic(screen, screen_size, frames, frame_loc, curFrame, fontB)
+		screen.blit(crosshairs[1],(x,10))
+		screen.blit(crosshairs[0],(screen_size[0]-(frame_size[0]+10),y))
+		screen.blit(crosshair_loc,(screen_size[0]-(frame_size[0]),screen_size[1]-100))
 	
+	# Adds all the points to the screen
 	if showPoints:
 		show_all_points(screen, keyFrames, frame_range, frame_loc, curFrame, dotR, dotB)
 	elif frames != [] and keyFrames[curFrame] != (1000,1000):
 		temp = keyFrames[curFrame]
 		screen.blit(dotR,(frame_loc[0]-5+temp[0],frame_loc[1]-5+temp[1]))
 	
+	# Displays a message if there is one and then clears it after 2 seconds
 	if message:
-		screen.blit(message_area,(15,600))
+		screen.blit(message_area,(15,590))
 		message_str = ""
 		message = False
 		timer = 0
@@ -154,7 +198,7 @@ while True:
 		if timer == 2*update_rate:
 			surface = pygame.Surface((200,40)) 
 			surface.fill((0,0,0))
-			screen.blit(surface,(15,600))
+			screen.blit(surface,(15,590))
 	
 	button_str = ""
 	pygame.display.update()
--- functions.py
+++ functions.py
@@ -1,3 +1,10 @@
+'''
+Author:   Matthew Donovan
+Created:  7/8/14
+File:     functions.py
+Purpose:  Provide functions for exp.py in a way to keep the driver file less cluttered
+'''	
+
 import pygame, os, sys, pickle
 from pygame.locals import *
 from button import *
@@ -8,16 +15,19 @@ from tkFileDialog import askopenfilename, asksaveasfilename
 global FPS
 FPS = 90
 
+# Steps a frame back
 def left_key(curFrame,frame_range):
 	if curFrame > frame_range[0]:
 		curFrame -= 1
 	return curFrame
 
+# Steps a frame forwards
 def right_key(curFrame,frame_range):
 	if curFrame < frame_range[1]:
 		curFrame += 1
 	return curFrame
-	
+
+# Manager for when a button is pressed and does the appropriate function
 def button_pressed(screen,curFrame,str,frame_range):
 	if str == "stepf":
 		return right_key(curFrame,frame_range)
@@ -40,7 +50,8 @@ def button_pressed(screen,curFrame,str,frame_range):
 		if infile != '':
 			cleanDir()
 			font = pygame.font.SysFont("sanserif",72)
-			label = font.render("CONVERTING",1,(200,0,0))
+			screen.blit(pygame.image.load("Images/pic_temp.png"),(720-(480+10),10))
+			label = font.render("CONVERTING",1,(255,255,255))
 			screen.blit(label,(310,300))
 			pygame.display.update()
 			os.system("mkdir pic_temp")
@@ -54,14 +65,14 @@ def button_pressed(screen,curFrame,str,frame_range):
 	elif str == "end":
 		return frame_range[1]
 		
-		
+# Switches out for the new video once the user selects to load another	
 def change_vid(infile, screen, font):
 	os.system("ffmpeg -i "+infile+" -r 25 -f image2 pic_temp/%05d.jpg")
 	#os.system("avconv -i "+infile+" -r 25 -f image2 pic_temp/%05d.jpg")
 	path, dirs, files = os.walk("./pic_temp/").next()
 	frames = []
 	length = len(files)
-	label = font.render("LOADING",1,(200,0,0))
+	label = font.render("LOADING",1,(255,255,255))
 	screen.blit(pygame.image.load("Images/pic_temp.png"),(720-(480+10),10))
 	screen.blit(label,(365,300))
 	bar = pygame.image.load("Images/loading.png")
@@ -75,50 +86,57 @@ def change_vid(infile, screen, font):
 			c += 1
 		v += 1
 	return frames
-	
+
+# Deletes the temp files placed in the directory and closes the program
 def exitAndClean():
 	cleanDir()
 	pygame.quit()
 	sys.exit()
-	
+
+# Deletes the temp files placed in the directory by the program
 def cleanDir():
 	os.system("rm -rf ./pic_temp")
-	
+
+# Loads the buttons into an array for easy access and processing	
 def load_buttons(buttons, screen, screen_size):
 	midx = screen_size[0] - 240; gap = 65; midy = 660; button_x = 50; button_y = 50; vertical = 150
 	buttons.append(Button(screen,False,None,None,"Images/load.png","Images/load2.png","load",(15,vertical+10),(200,63)))
-	buttons.append(Button(screen,False,None,None,"Images/start_frame.png","Images/start_frame2.png","set_start",(10,vertical+90),(100,38)))
-	buttons.append(Button(screen,False,None,None,"Images/finish_frame.png","Images/finish_frame2.png","set_end",(120,vertical+90),(100,38)))
+	buttons.append(Button(screen,False,None,None,"Images/start_frame.png","Images/start_frame2.png","set_start",(10,vertical+75),(100,38)))
+	buttons.append(Button(screen,False,None,None,"Images/finish_frame.png","Images/finish_frame2.png","set_end",(120,vertical+75),(100,38)))
+	buttons.append(Button(screen,False,None,None,"Images/reset.png","Images/reset2.png","reset",(15,vertical+145),(200,34)))
+	buttons.append(Button(screen,True,"Images/hide.png","Images/hide2.png","Images/show.png","Images/show2.png","show",(10,vertical+185),(100,38)))
+	buttons.append(Button(screen,False,None,None,"Images/clear.png","Images/clear2.png","clear",(120,vertical+185),(100,38)))
 	buttons.append(Button(screen,False,None,None,"Images/end.png","Images/end2.png","end",(midx+(2*gap),midy),(button_x,button_y)))
 	buttons.append(Button(screen,False,None,None,"Images/skipf.png","Images/skipf2.png","skipf",(midx+gap,midy),(button_x,button_y)))
 	buttons.append(Button(screen,False,None,None,"Images/stepf.png","Images/stepf2.png","stepf",(midx,midy),(button_x,button_y)))
 	buttons.append(Button(screen,False,None,None,"Images/stepb.png","Images/stepb2.png","stepb",(midx-gap,midy),(button_x,button_y)))
 	buttons.append(Button(screen,False,None,None,"Images/skipb.png","Images/skipb2.png","skipb",(midx-(2*gap),midy),(button_x,button_y)))
 	buttons.append(Button(screen,False,None,None,"Images/start.png","Images/start2.png","start",(midx-(3*gap),midy),(button_x,button_y)))
-	buttons.append(Button(screen,False,None,None,"Images/save.png","Images/save2.png","save",(15,650),(200,63)))
-	buttons.append(Button(screen,True,"Images/hide.png","Images/hide2.png","Images/show.png","Images/show2.png","show",(10,vertical+160),(100,38)))
-	buttons.append(Button(screen,False,None,None,"Images/clear.png","Images/clear2.png","clear",(120,vertical+160),(100,38)))
-	return buttons
+	buttons.append(Button(screen,False,None,None,"Images/save.png","Images/save2.png","save",(15,640),(200,63)))
 	
-def update_all(screen, screen_size, frames, frame_loc, frame_range, curFrame, font):
+	return buttons
+
+# Updates everything on the screen (except buttons)	
+def update_all(screen, screen_size, frames, frame_loc, numberBox, curFrame, font):
 	frame_num = font.render(str(curFrame),1,(255,255,255))
-	start_frame = font.render(str(frame_range[0]),1,(255,255,255))
-	end_frame = font.render(str(frame_range[1]),1,(255,255,255))
 	screen.blit(pygame.image.load("Images/bg.png"),(0,0))
 	screen.blit(pygame.image.load("Images/ORNL_Images/ORNL_Stacked_white_banner.png"),(0,0))
-	screen.blit(start_frame,(50,280))
-	screen.blit(end_frame,(155,280))
+	screen.blit(numberBox[0],(10,265))
+	screen.blit(numberBox[1],(120,265))
 	update_pic(screen, screen_size, frames, frame_loc, curFrame, font)
-	
+
+# Updates the buttons if they are being hovered over with the mouse	
 def update_buttons(screen, buttons):
 	for i in buttons:
 		screen.blit(i.getpicDisp(),(i.getx(),i.gety()))
 
+# Updates the video image and the frame number in the corner
 def update_pic(screen, screen_size, frames, frame_loc, curFrame, font):
 	frame_num = font.render(str(curFrame),1,(255,255,255))
 	screen.blit(frames[curFrame],frame_loc)
 	screen.blit(frame_num,(frame_loc[0]+5,frame_loc[1]+5))
-	
+
+# Plots all the points in the program
 def show_all_points(screen, keyFrames, frame_range, frame_loc, curFrame, dotR, dotB):
 	for i in range(frame_range[0], frame_range[1]):
 		p = keyFrames[i]
@@ -128,14 +146,16 @@ def show_all_points(screen, keyFrames, frame_range, frame_loc, curFrame, dotR, d
 				screen.blit(dotB,(frame_loc[0]-5+temp[0],frame_loc[1]-5+temp[1]))
 			else:
 				screen.blit(dotR,(frame_loc[0]-5+temp[0],frame_loc[1]-5+temp[1]))
-	
+
+# Deletes all the point by replacing them with (1000,1000) which can't happen on a 480x640 image
 def clear_points(keyFrames):
 	length = len(keyFrames)
 	keyFrames = []
 	for i in range(length):
 		keyFrames.append((1000,1000))
 	return keyFrames
-	
+
+# Saves a file assuming that all points in the range have been selected
 def save_file(keyFrames, frame_range, curFrame, font):
 	Tk().withdraw()
 	savefile = asksaveasfilename(filetypes=[("Python Pickle","*.p")])
@@ -161,7 +181,8 @@ def save_file(keyFrames, frame_range, curFrame, font):
 	
 	message_area = render_textrect(message_str, font, message_rect, (255,0,0), (0,0,0), justification=1)
 	return message_area, message, curFrame
-	
+
+# Checks to see if the user added the .p file extension. If not, this will add it to the end
 def check_filename(filename):
 	temp = filename.split(".")
 	if temp[len(temp)-1] == 'p':
