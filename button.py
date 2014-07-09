48e5a94e03b51efe5c69be699db776ac72d870f5
mjdonovan410 <mjdonovan410@gmail.com>
2014-07-09 10:41:08 -0400

    Updated Comments

--- button.py
+++ button.py
@@ -1,6 +1,15 @@
+'''
+Author:   Matthew Donovan
+Created:  7/8/14
+File:     button.py
+Purpose:  Provide a simple way to add buttons with ease to a program
+'''	
+
 import pygame, sys
 from pygame.locals import *
 
+# Takes the image paths and loads the images as variables so it can switch between the two on hover
+# If it is a toggle button, it will switch to the next two images when clicked
 class Button:
 	def __init__(self, screen, toggle, picpath1, picpath2, picpath3, picpath4, actionStr, loc, size):
 		if toggle:
@@ -17,7 +26,8 @@ class Button:
 		self.size = size
 		self.toggle = toggle
 		self.switch = False
-
+	
+	# Switches images when hovered over depending on which images it was previously when toggle button
 	def mouseloc(self,x,y):
 		if self.switch:
 			if x in range(self.loc[0],self.loc[0]+self.size[0]) and y in range(self.loc[1],self.loc[1]+self.size[1]):
